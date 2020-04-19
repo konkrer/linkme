07c28d738c9711b883ffb87e6d5b77f31e0ca304
Richard Iannucelli <44486701+konkrer@users.noreply.github.com>
2020-04-19 00:30:37 -0700

    removed username / password gibberish left in accidentally

--- linkme.py
+++ linkme.py
@@ -33,8 +33,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 # download chromedriver to local directory an enter local path below.
 CHROME_DRIVER_LOCATION = r""
 # LinkedIn credentials
-USERNAME = "asdasfasd"
-PASSWORD = "asdfsafd"
+USERNAME = ""
+PASSWORD = ""
 # Enter correct school or group name.
 SCHOOL = 'Springboard'
 
